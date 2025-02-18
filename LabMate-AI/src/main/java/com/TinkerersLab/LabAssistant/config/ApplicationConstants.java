706865524cfbca31f92038be6f5b5763ea2a5152
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2025-02-18 14:52:59 +0530

    adding functionality to vectorize txt files

--- LabMate-AI/src/main/java/com/TinkerersLab/LabAssistant/config/ApplicationConstants.java
+++ LabMate-AI/src/main/java/com/TinkerersLab/LabAssistant/config/ApplicationConstants.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 
 public class ApplicationConstants {
 
-    public final static String[] DEFAULT_RESOURCE_PATH = new String[] { "/opt/docs", "/opt/docs2" };
+    public final static String[] DEFAULT_RESOURCE_PATH = new String[] { "/opt/docs/pdf", "/opt/docs/text" };
 
     public static final String DEFAULT_HASH_ALGORITHM = "SHA-256";
 
     public static HashMap<String, String> INGESTION_RECORD;
 
-    public static final String DEFAULT_INGESTION_RECORD_PATH = "ingestion_record.dat";
+    public static final String DEFAULT_INGESTION_RECORD_PATH = "/home/happypotter/ingestion_record.dat";
 }
--- LabMate-AI/src/main/java/com/TinkerersLab/LabAssistant/service/IngestionService.java
+++ LabMate-AI/src/main/java/com/TinkerersLab/LabAssistant/service/IngestionService.java
@@ -1,8 +1,13 @@
 package com.TinkerersLab.LabAssistant.service;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
 
+import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
@@ -14,6 +19,7 @@ import org.springframework.stereotype.Service;
 import com.TinkerersLab.LabAssistant.config.ApplicationConstants;
 import com.TinkerersLab.LabAssistant.util.Utils;
 
+import ch.qos.logback.core.subst.Token;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -44,9 +50,22 @@ public class IngestionService {
                 continue;
             }
 
-            var pdfReader = new PagePdfDocumentReader(resource);
-            TextSplitter textSplitter = new TokenTextSplitter();
-            vectorStore.accept(textSplitter.apply(pdfReader.get()));
+            if (file.getName().toLowerCase().endsWith(".pdf")) {
+
+                var pdfReader = new PagePdfDocumentReader(resource);
+                TextSplitter textSplitter = new TokenTextSplitter();
+                vectorStore.accept(textSplitter.apply(pdfReader.get()));
+
+            } else if (file.getName().toLowerCase().endsWith(".txt")) {
+                try {
+                    String content = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+                    TextSplitter textSplitter = new TokenTextSplitter();
+
+                    vectorStore.accept(textSplitter.apply(List.of(new Document(content))));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
             ApplicationConstants.INGESTION_RECORD.put(file.getName(), utils.getFileHash(file));
             log.info("Vector store loaded with " + file.getName());
